1062ffe3411e0a7edf71eff8295319880654ad04
Kevin Lee <kevinmlee.x@gmail.com>
2019-05-08 16:06:38 -0700

    redirect landing to photos

--- src/Landing.react.js
+++ src/Landing.react.js
@@ -1,7 +1,12 @@
 // @flow
 
 import * as React from 'react';
+import {Redirect} from 'react-router-dom';
 
-const Landing = () => <div className="landing" />;
+const Landing = () => (
+  <div className="landing">
+    <Redirect to="/photo" />
+  </div>
+);
 
 export default Landing;
