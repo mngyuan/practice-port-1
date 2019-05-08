b091e12dde09f604b01db85e43937226552392bf
Kevin Lee <kevinmlee.x@gmail.com>
2019-05-08 16:20:57 -0700

    use router with proper base URL

--- src/App.js
+++ src/App.js
@@ -1,59 +1,55 @@
 // @flow
-
+//
+import {createBrowserHistory} from 'history';
 import * as React from 'react';
-import {Link, Route, withRouter} from 'react-router-dom';
+import {Link, Route, BrowserRouter, Switch, withRouter} from 'react-router-dom';
 import Landing from './Landing.react';
 import Photo from './Photo.react';
 import Print from './Print.react';
 import Video from './Video.react';
 import About from './About.react';
 
+const history = createBrowserHistory();
+
+const Content = withRouter(props => (
+  <div className="container">
+    <div className="content">
+      <div className="content-anchor">
+        <div className="content-scrollable">
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/photo/:set?" component={Photo} />
+            <Route path="/about" exact component={About} />
+            <Route path="/video" exact component={Video} />
+            <Route path="/print/:set?" component={Print} />
+          </Switch>
+        </div>
+        <nav className="left-bar">
+          <Link to="/about">ABOUT</Link>
+          <Link to="/print">PRINT</Link>
+          <Link to="/photo">PHOTO</Link>
+          <Link to="/video">VIDEO</Link>
+        </nav>
+        <div className="right-bar">
+          <h2>{props.location.pathname.split('/')[1]}</h2>
+        </div>
+        <div className="top-bar">
+          <h1>MNGYUAN</h1>
+        </div>
+        <div className="bottom-bar">COPYRIGHT 2019</div>
+      </div>
+    </div>
+  </div>
+));
+
 class App extends React.PureComponent {
-  state = {
-    background: 'black',
-  };
-
-  _onMouseMove = e => {
-    //const x = Math.floor((e.screenX / window.innerWidth) * 128 + 127);
-    //const y = Math.floor((e.screenY / window.innerHeight) * -128 + 127);
-    //console.log(x, y);
-    //this.setState({background: `rgba(${x}, ${y}, 187, 0.4)`});
-  };
-
   render() {
     return (
-      <div
-        className="container"
-        onMouseMove={this._onMouseMove}
-        style={{background: this.state.background}}
-      >
-        <div className="content">
-          <div className="content-anchor">
-            <div className="content-scrollable">
-              <Route path="/" exact component={Landing} />
-              <Route path="/photo/:set?" component={Photo} />
-              <Route path="/about" exact component={About} />
-              <Route path="/video" exact component={Video} />
-              <Route path="/print/:set?" component={Print} />
-            </div>
-            <nav className="left-bar">
-              <Link to="/about">ABOUT</Link>
-              <Link to="/print">PRINT</Link>
-              <Link to="/photo">PHOTO</Link>
-              <Link to="/video">VIDEO</Link>
-            </nav>
-            <div className="right-bar">
-              <h2>{this.props.location.pathname.split('/')[1]}</h2>
-            </div>
-            <div className="top-bar">
-              <h1>MNGYUAN</h1>
-            </div>
-            <div className="bottom-bar">COPYRIGHT 2019</div>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter basename={process.env.PUBLIC_URL} history={history}>
+        <Content />
+      </BrowserRouter>
     );
   }
 }
 
-export default withRouter(App);
+export default App;
