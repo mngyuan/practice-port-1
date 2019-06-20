ef7ab66e1fafeb100508d18fef8d9d07bbbe89bd
Kevin Lee <kevinmlee.x@gmail.com>
2019-06-20 16:05:39 -0700

    watery movement on mousemove

--- src/index.js
+++ src/index.js
@@ -15,3 +15,15 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root'),
 );
+
+document.documentElement.style.setProperty('--mouse-x', 0.5);
+document.documentElement.style.setProperty('--mouse-y', 0.5);
+document.addEventListener('mousemove', e => {
+  const mouseXPerc = e.clientX / window.innerWidth;
+  const mouseYPerc = e.clientY / window.innerHeight;
+  const mouseXVal = Math.log((Math.E - 1) * mouseXPerc + 1);
+  const mouseYVal = Math.log((Math.E - 1) * mouseYPerc + 1);
+  document.documentElement.style.setProperty('--mouse-x', mouseXVal);
+  document.documentElement.style.setProperty('--mouse-y', mouseYVal);
+  console.log(mouseXVal, mouseYVal);
+});
